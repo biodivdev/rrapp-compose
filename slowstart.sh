60fcbb69ef6c39b01ce71b06319770675777bfdf
Diogo Silva <diogo@diogok.net>
2016-01-20 12:40:03 -0200

    slowstart

---
+++ slowstart.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+docker-compose up -d proxy
+
+docker-compose up -d taxadata
+docker-compose up -d dwcservices
+docker-compose up -d dwcbot
+sleep 5
+docker-compose up -d elasticsearch
+sleep 15
+docker-compose up -d kibana
+docker-compose up -d biodividx
+docker-compose up -d biodivui
+
